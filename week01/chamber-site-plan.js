db2beb77f645977567857225f21a97514fceea40
NKO22 <nge21003@byui.edu>
2023-06-11 22:29:43 -0600

    add to home page

---
+++ week01/chamber-site-plan.js
@@ -0,0 +1,14 @@
+// JavaScript code to dynamically populate the current year and last modified date
+
+document.addEventListener('DOMContentLoaded', function() {
+    var yearElem = document.getElementById('year');
+    var lastModifiedElem = document.getElementById('lastModified');
+  
+    // Populate current year
+    var currentYear = new Date().getFullYear();
+    yearElem.textContent = currentYear;
+  
+    // Populate last modified date
+    var lastModifiedDate = document.lastModified;
+    lastModifiedElem.textContent = 'Last Modified: ' + lastModifiedDate;
+  });
