a6fc4308c48883d23a11693b1bc7d0223973445e
NKO22 <nge21003@byui.edu>
2023-07-02 16:10:11 -0600

    add to home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,9 @@
-let newDate = new Date();
 
-document.getElementById("currentYear").textContent = newDate.getFullYear();
+// document.write(new Date().getFullYear())
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("newDate").innerHTML = year;
 
-let lastUpdated = document.lastModified;
 
-document.getElementById("updatedDate").textContent = lastUpdated;
+const date = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = date; 
