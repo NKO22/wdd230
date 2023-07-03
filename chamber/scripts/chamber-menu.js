cf71b41f8d03a28acb1b2b8e5d92e07e26aec947
NKO22 <nge21003@byui.edu>
2023-07-02 21:02:18 -0600

    add to home page

---
+++ chamber/scripts/chamber-menu.js
@@ -0,0 +1,7 @@
+const mybtn = document.querySelector("header button");
+const mynav = document.querySelectorAll("nav");
+
+mybtn.addEventListener("click", () => {
+	mynav.forEach((item) => item.classList.toggle("open"));
+	mybtn.classList.toggle("close");
+});
