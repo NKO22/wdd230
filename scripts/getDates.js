234876dd3ac8b8f449e3481dde147a40263a1bfd
NKO22 <nge21003@byui.edu>
2023-07-13 11:41:32 -0600

    add to home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,9 +1,37 @@
+// Get the current year
+const yearSpan = document.getElementById('year');
+yearSpan.textContent = new Date().getFullYear();
 
-// document.write(new Date().getFullYear())
-const d = new Date();
-let year = d.getFullYear();
-document.getElementById("newDate").innerHTML = year;
+// Get the last modified date of the document
+const lastModifiedSpan = document.getElementById('lastModified');
+lastModifiedSpan.textContent = 'Last Modified: ' + document.lastModified;
 
 
-const date = new Date(document.lastModified);
-document.getElementById("lastModified").innerHTML = date; 
+//validate password
+
+var password = document.getElementById("password") , confirm_password = document.getElementById("confirm_password");
+
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+
+//range bar
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
