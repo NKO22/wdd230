b2be0debc477a48e55a66730b4070dcb028e3f60
NKO22 <nge21003@byui.edu>
2023-07-19 17:04:19 -0600

    add to home page

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,10 +1,35 @@
-const yearSpan = document.getElementById("year");
-const lastModifiedParagraph = document.getElementById("lastModified");
+// Get the current year
+const yearSpan = document.getElementById('year');
+yearSpan.textContent = new Date().getFullYear();
 
-// Populate the year
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
+// Get the last modified date of the document
+const lastModifiedSpan = document.getElementById('lastModified');
+lastModifiedSpan.textContent = 'Last Modified: ' + document.lastModified;
 
-// Populate the last modified date
-const lastModifiedDate = new Date(document.lastModified);
-lastModifiedParagraph.textContent = "Last modified: " + lastModifiedDate.toDateString();
+// Get the timestamp
+function setCurrentTimestamp() {
+    var timestampField = document.getElementById("timestamp");
+    var currentTimestamp = new Date().toISOString();
+    timestampField.value = currentTimestamp;
+  }
+
+  // Call the function when the form is loaded
+  window.addEventListener("load", setCurrentTimestamp);
+
+  
+
+//Weather date
+const yearSpan_1 = document.getElementById('weather-day');
+const yearSpan_2 = document.getElementById('weather-day-2');
+const yearSpan_3 = document.getElementById('weather-day-3');
+
+
+
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let d = new Date();
+let dayName = days[d.getDay()];
+let dayName_2 = days[d.getDay()+1];
+let dayName_3 = days[d.getDay()+2];
+yearSpan_1.textContent = dayName;
+yearSpan_2.textContent = dayName_2;
+yearSpan_3.textContent = dayName_3;
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,43 +1,86 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const foreCast = document.querySelector('#foreCast');
 const captionDesc = document.querySelector('figcaption');
 
+const currentTemp_2 = document.querySelector('#current-temp-2');
+const weatherIcon_2 = document.querySelector('#weather-icon-2');
+const captionDesc_2 = document.querySelector('#caption-2');
 
-// Declare a const variable name "url" and assign it a valid URL string
-const url = 'https://api.openweathermap.org/data/2.5/weather';
+const currentTemp_3 = document.querySelector('#current-temp-3');
+const weatherIcon_3 = document.querySelector('#weather-icon-3');
+const captionDesc_3 = document.querySelector('#caption-3');
 
-
-// Use the Current Weather API and start a query string
-const apiKey = '15a1ab955380b7a83ec1c9828e8bc826';
-const city = 'McCall';
-const units = 'imperial';
-const queryString = `?q=${city}&appid=${apiKey}&units=${units}`;
-
-const fullURL = url + queryString;
+const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=37.09&lon=-113.56&units=imperial&appid=00fe2db85081e760045e7f549842c9fe';
 
 async function apiFetch() {
-  try {
-    const response = await fetch(fullURL);
-    if (response.ok) {
-      const data = await response.json();
-      //console.log(data); // testing only
-      displayResults(data); // uncomment when ready
-    } else {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
         throw Error(await response.text());
-    }
-  } catch (error) {
+      }
+    } catch (error) {
       console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(data) {
+    currentTemp.innerHTML = `${data.list[0].main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
+    let desc = data.list[0].weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+
+    currentTemp_2.innerHTML = `${data.list[1].main.temp}&deg;F`;
+    const iconsrc_2 = `https://openweathermap.org/img/w/${data.list[8].weather[0].icon}.png`;
+    let desc_2 = data.list[8].weather[0].description;
+    weatherIcon_2.setAttribute('src', iconsrc_2);
+    weatherIcon_2.setAttribute('alt', desc_2);
+    captionDesc_2.textContent = `${desc_2}`;
+
+    currentTemp_3.innerHTML = `${data.list[2].main.temp}&deg;F`;
+    const iconsrc_3 = `https://openweathermap.org/img/w/${data.list[16].weather[0].icon}.png`;
+    let desc_3 = data.list[16].weather[0].description;
+    weatherIcon_3.setAttribute('src', iconsrc_3);
+    weatherIcon_3.setAttribute('alt', desc_3);
+    captionDesc_3.textContent = `${desc_3}`;
+  }
+
+
+  
+  
+  
+//Banner
+// Check if the current day is Monday, Tuesday, or Wednesday
+function isWeekday() {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  return dayOfWeek >= 1 && dayOfWeek <= 3; // Monday: 1, Tuesday: 2, Wednesday: 3
+}
+
+// Display the banner on weekdays
+function showBanner() {
+  const banner = document.getElementById('header');
+  if (isWeekday()) {
+    banner.style.display = 'block';
   }
 }
 
-apiFetch();
+// Hide the banner when the "Close" button is clicked
+function hideBanner() {
+  const banner = document.getElementById('header');
+  banner.style.display = 'none';
+}
 
-function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  let desc = data.weather[0].description;
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = `${desc}`;
-} 
+// Event listener for the "Close" button
+const closeBtn = document.getElementById('btn-bgstroke');
+closeBtn.addEventListener('click', hideBanner);
+
+// Show the banner when the page loads
+window.addEventListener('load', showBanner);
